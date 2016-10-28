ad4437625abf485caed77d5b093c4bad2e21021b
Sofyan Thayf <sofyanthayf@gmail.com>
2016-10-28 11:50:45 +0800

    lupa JQuery-nya
    link untuk JQuery ditambahkan dalam view/templates/footer.php
    Belum ada hubungan dengan Firebase, hanya untuk support template
    bootstrap untuk tampilan

--- guestbook/application/views/templates/footer.php
+++ guestbook/application/views/templates/footer.php
@@ -1,5 +1,12 @@
 	</div>
+
 	
+	<!-- jQuery -->
+	<script
+		src="https://code.jquery.com/jquery-3.1.1.min.js"
+		integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+		crossorigin="anonymous"></script>
+
 	<script src="assets/js/bootstrap.min.js"></script>
 </body>
 </html>
