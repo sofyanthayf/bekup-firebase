c40dd635de70b405bcd042395aa5606c047d61c9
Sofyan Thayf <sofyanthayf@gmail.com>
2016-10-29 11:42:44 +0800

    little bit style
    penambahan gbook.css untuk sedikit merapikan tampilan front-end
    plus cadangan kode js yang agak panjang

---
+++ guestbook/assets/js/gbook.js
@@ -0,0 +1,17 @@
+
+
+/* dicadangkan karena agak panjang :) */
+/*
+		$('#posts:last-child').append(
+									   "<tr>\
+											<td>\
+												<div class='dispnama'>"+ nama +"</div>\
+												<div class='dispmail'>"+ email +"</div>\
+												<div class='disptgl'>"+ dtgl + "</div>\
+											</td>\
+											<td>\
+												<div class='dispmess'>"+ pesan +"</div>\
+											</td>\
+										</tr>"
+									);
+*/
